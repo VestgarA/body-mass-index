98c13524d3f388af5d62108f3bcf3ae9455e2f46
vestgar <sid06@bk.ru>
2025-04-18 13:49:14 +0300

    new commit

--- src/bmiServise.java
+++ src/bmiServise.java
@@ -1,7 +1,7 @@
 public class bmiServise {
     public double counting(double bodyWeight, double height) {
 
-        double resalt = 98 / (1.87 * 1.87);
+        double resalt = bodyWeight / (height * height);
         return resalt;
     }
 }
